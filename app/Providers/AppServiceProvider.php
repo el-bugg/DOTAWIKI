96e3b54f41433c456375c6d1e43f1c47578d10ba
el-bugg <zakialdrin@gmail.com>
2026-01-12 11:47:45 +0700

    fix duplicate match_data column error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA TAMBAHKAN BARIS INI
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Paksa semua link (CSS/Gambar) jadi HTTPS jika sedang di Production
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
